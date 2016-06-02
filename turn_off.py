bf1439fb2453e17f585a0c93e8d3b704beab0cf5
Nate Fox <nate.m.fox@gmail.com>
2016-06-01 20:28:32 -0700

    adding off/on files

---
+++ turn_off.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+import time
+
+IPADDR = '192.168.1.150'
+PORTNUM = 9999
+
+# FROM ON TO OFF
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IPADDR, PORTNUM))
+data = '0000002ad0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381f286e793f6d4eedea3dea3'.decode('hex')
+s.send(data)
+data = '0000002dd0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381e496e4bbd8b7d3b694ae9ee39ee3'.decode('hex')
+s.send(data)
+s.close
+
---
+++ turn_on.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import socket
+import time
+
+IPADDR = '192.168.1.150'
+PORTNUM = 9999
+
+## FROM OFF TO ON
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IPADDR, PORTNUM))
+data = '0000002ad0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381f286e793f6d4eedfa2dfa2'.decode('hex')
+s.send(data)
+time.sleep(1)
+
+data = '00000025d0f281e28aef8bfe92f7d5ef94b6d1b4c09ff194ec98c7a6c5b1d8b7d9fbc1afdab6daa7da'.decode('hex')
+s.send(data)
+s.close()
+
